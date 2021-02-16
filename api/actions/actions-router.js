1f29a263b14b047ef4a75dc83a7e1a9ce06ae6fe
Plofland <ploflan@gmail.com>
2021-02-15 21:43:20 -0500

    Added validateId middleware, GET actions and GET actions by id functional

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,26 +1,38 @@
 const express = require('express');
 const actionsFunc = require('./actions-model');
+const { validateId } = require('../middleware/middleware');
 
 const router = express.Router();
 
-//*Write out the endpoints that I'll need before writing out the code for each route
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await actionsFunc.get();
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/', (req, res) => {
-  //returns an array of actions (or an empty array) as the body of the response.
-})
-
-router.get('/:id', (req, res) => {
-  //returns an action with the given id as the body of the response.
-})
+router.get('/:id', validateId, async (req, res) => {
+  res.status(200).json(req.actionId);
+});
 
 router.post('/', (req, res) => {
-  //returns the newly created action as the body of the response.
-})
+  //!returns the newly created action as the body of the response.
+});
 
 router.put('/:id', (req, res) => {
-  //returns the updated action as the body of the response.
-})
+  //!returns the updated action as the body of the response.
+});
 
 router.delete('/:id', (req, res) => {
-  //returns no response body.
-})
+  //!returns no response body.
+});
+
+router.use((error, req, res, next) => {
+  res
+    .status(500)
+    .json({ message: error.message, stack: error.stack });
+});
+
+module.exports = router;
--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -2,3 +2,29 @@ const projectsFunc = require('../projects/projects-model');
 const actionsFunc = require('../actions/actions-model');
 
 //*Plan out the middleware that I will be needing before I write out anything
+//todo: validat ID, validate action to be posted to server, validate project to be posted to server
+
+const validateId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const actionId = await actionsFunc.get(id);
+    if (!actionId) {
+      res.status(400).json({
+        message: `No action with the id of ${id}`
+      });
+    } else {
+      req.actionId = actionId;
+      next();
+    }
+  } catch (error) {
+    res.status(500).json(`Server error: ${error}`);
+  }
+};
+
+// const validActionsPost = async (req, res, next) => {
+
+// }
+
+module.exports = {
+  validateId
+};
